0f2733b9188d6f530671b2562b5ad34948beece8
Luana Beatriz Souza Venâncio <luanavenancio73643@gmail.com>
2024-09-10 20:32:26 -0300

    Initial commit

---
+++ ProcessamentoImagens/frmImagem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProcessamentoImagens
+{
+    public partial class frmImagem : Form
+    {
+        private Bitmap imageBitmap;
+        public frmImagem(Bitmap imageBitmap)
+        {
+            InitializeComponent();
+
+            this.imageBitmap = imageBitmap;
+        }
+
+        private void frmImagem_Load(object sender, EventArgs e)
+        {
+            pictureBox.Image = imageBitmap;
+            pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+    }
+}
